ee19d26f3fdbe19d46bc5409ef79d1c94cbdc9c4
lava <szymonole2009@gmail.com>
2025-05-24 12:58:54 +0200

    Dodaj pliki projektów.

---
+++ Pasjans/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Pasjans
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ustawienie enkodowania aby dzialaly znaki specjalne
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Wczytanie ascii artu kart z pliku
+            string[] lines = File.ReadAllLines(@"..\..\cardArt.txt");
+
+            // Stworzenie tablicy list do przechowywania wstepnych miejsc kart
+            List<int>[] stackPlaces = new List<int>[7];
+            for (int i = 0; i < 7; i++)
+            {
+                stackPlaces[i] = new List<int>();
+            }
+
+            // Losowanie kart w dane miejsca
+            Random r = new Random();
+            for (int i = 0; i < 7; i++)
+            { 
+                for (int j = i; j < 7; j++)
+                {
+                    stackPlaces[i].Add(r.Next(0, 52));
+                }
+            }
+
+            // Stworzenie stosow kart
+            Stack[] stacks = new Stack[7];
+            for (int i = 0;i < 7; i++)
+            {
+                stacks[i] = new Stack(stackPlaces[i], 1);
+            }
+
+            stacks[0].exposed = 4;
+
+            // Pierwsze wyrysowanie kart
+            Draw(lines, stacks, "Wykonaj ruch.");
+
+            string info = "";
+
+            stacks[0].PrintStack();
+
+            while (true)
+            {
+                Console.Clear();
+                Draw(lines, stacks, info);
+
+                string input = Console.ReadLine();
+
+                string[] inputElements = input.Split(' ');
+
+                if (inputElements[0] == "m" || inputElements[0] == "move")
+                {
+                    if (inputElements.Length == 4 &&
+                        int.TryParse(inputElements[1], out int stackOne) && 
+                        int.TryParse(inputElements[2], out int amount) && 
+                        int.TryParse(inputElements[3], out int stackTwo))
+                    {
+                        info = MoveCard(stacks, stackOne - 1, stackTwo - 1);
+                    }
+                    else
+                        info = "Nieprawidlowe wejscie.";
+                }
+                else if (inputElements[0] == "rem")
+                {
+                    stacks[0].DeleteTop(1);
+                    info = "Usunięto kartę.";
+                }
+                else if (inputElements[0] == "exit")
+                    break;
+                else
+                    info = "Niepoprawne wejście.";
+            }
+        }
+
+        static string MoveCard(Stack[] stacks, int stackOne, int stackTwo)
+        {
+            int[] cards = stacks[stackOne].GetTopCards(2);
+            int cardTwo = stacks[stackTwo].GetTopCards(1)[0];
+            int valueOne = cards[cards.Length - 1] / 4;
+            int valueTwo = cardTwo / 4;
+            int suitOne = cards[cards.Length - 1] % 4;
+            int suitTwo = cardTwo % 4;
+
+            //if (valueOne == valueTwo - 1 && ((suitOne >= 2 && suitTwo < 2) || (suitTwo >= 2 && suitOne < 2)))
+            //{
+
+            Debug.WriteLine($"Przesuwam z talii {stackOne} do {stackTwo}");
+            Debug.WriteLine("Przed ruch:");
+            stacks[stackOne].PrintStack();
+            stacks[stackTwo].PrintStack();
+
+            stacks[stackOne].DeleteTop(2);
+            stacks[stackTwo].AddCards((int[])cards.Clone());
+            //stacks[stackOne].exposed -= 3;
+            stacks[stackTwo].exposed += 2;
+
+            Debug.WriteLine("Po ruchu:");
+            stacks[stackOne].PrintStack();
+            stacks[stackTwo].PrintStack();
+
+            return "Przesunięto kartę.";
+            //}
+
+            //return "Nieprawidłowy ruch.";
+
+        }
+
+        // Rysowanie w konsoli stosow
+        static void Draw(string[] lines, Stack[] stacks, string info)
+        {
+            // Tworzenie tablicy list do przechowywania linii do wydrukowania otrzymanych od stosow
+            List<string>[] stackLines = new List<string>[7];
+            for (int i = 0; i < 7; i++)
+            {
+                stackLines[i] = new List<string>();
+            }
+
+            // Pobieranie od stosow linii do wyprintowania
+            for (int i = 0; i < 7; i++)
+            {
+                stackLines[i] = stacks[i].GenerateDrawLines(lines);
+            }
+
+            // Printowanie linii w konsoli
+            for (int i = 0; i < 25; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write(stackLines[j][i] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(info);
+
+            foreach (Stack stack in stacks)
+            {
+                Console.Write(stack.places.Count + "," + stack.exposed + ";");
+            }
+            Console.WriteLine();
+
+            stacks[0].PrintStack();
+
+            Console.Write("> ");
+        }
+    }
+}
---
+++ Pasjans/Stack.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pasjans
+{
+    internal class Stack
+    {
+        // Tworzenie listy na przechowywanie kart jakie sa na stosie
+        public List<int> places = new List<int>();
+
+        public int exposed;
+
+        // Preset pustego miejsca
+        const string emptyPreset = "           ";
+        const string topPreset = "┌─────────┐";
+
+        // Konstruktor, pobieranie miejsc przy tworzeniu stosu
+        public Stack(List<int> places, int exposed) 
+        {
+            this.places = places;
+            this.exposed = exposed;
+        }
+
+        // Funkcja zwracajaca linie jakie nalezy wydrukowac w konsoli
+        public List<string> GenerateDrawLines(string[] lines)
+        {
+            // Zmienna do przechowywania linii
+            List<string> drawLines = new List<string>();
+            
+            // Jesli stos jest pusty zwracamy 20 pustych linii
+            if (places.Count == 0)
+            {
+                for (int i = 0; i < 25; i++)
+                {
+                    drawLines.Add(emptyPreset);
+                }
+                return drawLines;
+            }
+
+            // Najpierw dajemy linie wystajace gory kart zakrytych
+            for (int i = 0; i < places.Count - exposed; i++)
+            {
+                drawLines.Add(topPreset);
+            }
+            for (int i = 0; i < exposed - 1; i++)
+            {
+                drawLines.Add(topPreset);
+                drawLines.Add(lines[places[exposed - 1 + i] * 7 + 1]);
+            }
+            // Potem dajemy linie karty ktora jest odslonieta pobrane z pliku
+            for (int i = 0; i < 7; i++)
+            {
+                drawLines.Add(lines[places[0] * 7 + i]);
+            }
+            // Reszte linii dajemy puste aby razem bylo 20
+            for (int i = 0; i < 25 - (places.Count + 5 + exposed); i++)
+            {
+                drawLines.Add(emptyPreset);
+            }
+
+            return drawLines;
+        }
+
+        public int GetExposed()
+        {
+            return exposed;
+        }
+
+        public int[] GetTopCards(int amount)
+        {
+            int[] cards = new int[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                cards[i] = places[i];
+            }
+            return cards;
+        }
+
+        // Funkcja usuwajaca gorna karte stosu
+        public void DeleteTop(int amount)
+        {
+            if (exposed > amount)
+                exposed -= amount;
+            for (int i = 0; i < amount; i++)
+            {
+                places.RemoveAt(0);
+            }
+        }
+
+        public void AddCards(int[] cards)
+        {
+
+            for (int i = cards.Length - 1; i >= 0; i--)
+            {
+                places.Insert(0, cards[i]); 
+            }
+
+            Debug.WriteLine("Dodaję do talii:");
+            foreach (var c in cards)
+                Debug.Write(c + " ");
+            Debug.WriteLine("");
+        }
+
+        public void PrintStack()
+        {
+            foreach (int i in places)
+            {
+                Debug.Write(i + " ");
+            }
+            Debug.WriteLine("");
+        }
+    }
+}
